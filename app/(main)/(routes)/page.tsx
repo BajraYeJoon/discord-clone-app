a851b13917bfd55839f13f017c9c55a693f07af3
bajrayejoon <binaya.bajrashakya@gmail.com>
2023-09-01 11:25:02 +0545

    chore: update folder setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-      <p className="text-3xl font-bold text-indigo-500">Hello World!</p>
-      <Button>Click Me</Button>
-    </div>
-  );
+  return <div>Protected Page</div>;
 }
